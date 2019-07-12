f4b19aead1d7d2dacae80c123dd16b6386dcbdd3
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-12 16:07:15 -0700

    Adding routes directory and adding html-routes.js within directory

---
+++ routes/html-routes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/pref.html"));
+    });
+    
+    app.get("/main", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/main.html"));
+    });
+}

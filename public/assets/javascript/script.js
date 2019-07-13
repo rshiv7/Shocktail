68abcec99c277e4f1765de7e5fbc00ae0177a445
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-07-12 20:56:36 -0700

    added getIngredients function

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -17,36 +17,33 @@ function searchByName(event) {
     $(".coctailsList").empty();
     console.log(response);
     var drinks = response.drinks;
-    for (var i = 0; i < drinks.length; i++) {
-      var drinkDiv = $("<div>").addClass("card");
+    if (drinks == "None Found") {
+      var img = $(".img").attr("src", "sad-cartoon-margarita.png");
+      $(".coctailsList")
+        .empty()
+        .html("<h4>None Found</h4>")
+        .append(img);
+    } else {
+      for (var i = 0; i < drinks.length; i++) {
+        var drink = drinks[i];
+        var drinkDiv = $("<div>").addClass("card");
 
-      var cardImgDiv = $("<div>").addClass("card-image");
-      var drinkImg = $("<img>").attr("src", drinks[i].strDrinkThumb);
-      cardImgDiv.append(drinkImg);
+        var cardImgDiv = $("<div>").addClass("card-image");
+        var drinkImg = $("<img>").attr("src", drink.strDrinkThumb);
+        cardImgDiv.append(drinkImg);
 
-      var cardContentDiv = $("<div>").addClass("card-content");
-      var drinkName = $("<span>")
-        .addClass("card-title")
-        .text(drinks[i].strDrink);
+        var cardContentDiv = $("<div>").addClass("card-content");
+        var drinkName = $("<span>")
+          .addClass("card-title")
+          .text(drink.strDrink);
 
-      var drinkRecipe = $("<p>").text(drinks[i].strInstructions);
-      var drinkIngredients = $("<div>").html("<b> Ingregients </b>");
-      for (var j = 1; j < 15; j++) {
-        var ingredient = drinks[i]["strIngredient" + j];
-        var measure = drinks[i]["strMeasure" + j];
-        if (ingredient && ingredient.length > 0) {
-          if (measure.length > 0) {
-            drinkIngredients.append(
-              "<li>" + ingredient + ": " + measure + "</li> "
-            );
-          } else {
-            drinkIngredients.append("<li>" + ingredient + "</li> ");
-          }
-        }
+        var drinkRecipe = $("<p>").text(drink.strInstructions);
+        var drinkIngredients = $("<div>").html("<b> Ingregients </b>");
+        getIngredients(drink, drinkIngredients);
       }
 
       cardContentDiv.append(drinkName, drinkRecipe, drinkIngredients);
-      $(drinkDiv).append(cardImgDiv, cardContentDiv);
+      drinkDiv.append(cardImgDiv, cardContentDiv);
 
       $(".coctailsList").append(drinkDiv);
     }
@@ -62,13 +59,14 @@ function showRandom() {
   }).then(function(response) {
     $(".coctailsList").empty();
     var drink = response.drinks[0];
+
     var drinkDiv = $("<div>").addClass("card");
 
     var cardImgDiv = $("<div>").addClass(
       "card-image waves-effect waves-block waves-light"
     );
     var drinkImg = $("<img>").attr("src", drink.strDrinkThumb);
-    $(cardImgDiv).append(drinkImg);
+    cardImgDiv.append(drinkImg);
 
     var cardContentDiv = $("<div>").addClass("card-content");
     var icon = $("<i>")
@@ -85,33 +83,36 @@ function showRandom() {
     var iconClose = $("<i>")
       .addClass("material-icons right")
       .text("close");
-    var cardRevealSpan = $("<span>").addClass(
-      "card-title grey-text text-darken-4"
-    );
-    $(cardRevealSpan)
+    var cardRevealSpan = $("<span>")
+      .addClass("card-title grey-text text-darken-4")
       .text(drink.strDrink)
       .append(iconClose);
 
     var drinkRecipe = $("<p>").text(drink.strInstructions);
     var drinkIngredients = $("<div>").html("<b> Ingregients </b>");
-    for (var j = 1; j < 15; j++) {
-      var ingredient = drink["strIngredient" + j];
-      var measure = drink["strMeasure" + j];
-      if (ingredient && ingredient.length > 0) {
-        if (measure.length > 0) {
-          drinkIngredients.append(
-            "<li>" + ingredient + ": " + measure + "</li> "
-          );
-        } else {
-          drinkIngredients.append("<li>" + ingredient + "</li> ");
-        }
-      }
-    }
-    $(cardRevealDiv).append(cardRevealSpan, drinkRecipe, drinkIngredients);
-    $(drinkDiv).append(cardImgDiv, cardContentDiv, cardRevealDiv);
+
+    getIngredients(drink, drinkIngredients);
+    cardRevealDiv.append(cardRevealSpan, drinkRecipe, drinkIngredients);
+    drinkDiv.append(cardImgDiv, cardContentDiv, cardRevealDiv);
 
     $(".coctailsList").append(drinkDiv);
   });
+}
+
+function getIngredients(drink, drinkIngredients) {
+  for (var j = 1; j < 15; j++) {
+    var ingredient = drink["strIngredient" + j];
+    var measure = drink["strMeasure" + j];
+    if (ingredient && ingredient.length > 0) {
+      if (measure.length > 0) {
+        drinkIngredients.append(
+          "<li>" + ingredient + ": " + measure + "</li> "
+        );
+      } else {
+        drinkIngredients.append("<li>" + ingredient + "</li> ");
+      }
+    }
+  }
 }
 
 function getInfo(id) {
@@ -128,48 +129,35 @@ function getInfo(id) {
       "card-image waves-effect waves-block waves-light"
     );
     var drinkImg = $("<img>").attr("src", drinks.strDrinkThumb);
-    $(cardImgDiv).append(drinkImg);
+    cardImgDiv.append(drinkImg);
 
     var cardContentDiv = $("<div>").addClass("card-content");
     var icon = $("<i>")
       .addClass("material-icons right")
       .text("more_vert");
 
-    var cardContentSpan = $("<span>").addClass(
-      "card-title activator grey-text text-darken-4"
-    );
-    cardContentSpan.text(drinks.strDrink).append(icon);
+    var cardContentSpan = $("<span>")
+      .addClass("card-title activator grey-text text-darken-4")
+      .text(drinks.strDrink)
+      .append(icon);
     cardContentDiv.append(cardContentSpan);
 
     var cardRevealDiv = $("<div>").addClass("card-reveal");
     var iconClose = $("<i>")
       .addClass("material-icons right")
       .text("close");
-    var cardRevealSpan = $("<span>").addClass(
-      "card-title grey-text text-darken-4"
-    );
-    $(cardRevealSpan)
+    var cardRevealSpan = $("<span>")
+      .addClass("card-title grey-text text-darken-4")
       .text(drinks.strDrink)
       .append(iconClose);
 
     var drinkRecipe = $("<p>").text(drinks.strInstructions);
     var drinkIngredients = $("<div>").html("<b> Ingregients </b>");
-    for (var j = 1; j < 15; j++) {
-      var ingredient = drinks["strIngredient" + j];
-      var measure = drinks["strMeasure" + j];
-      if (ingredient && ingredient.length > 0) {
-        if (measure.length > 0) {
-          drinkIngredients.append(
-            "<li>" + ingredient + ": " + measure + "</li> "
-          );
-        } else {
-          drinkIngredients.append("<li>" + ingredient + "</li> ");
-        }
-      }
-    }
 
-    $(cardRevealDiv).append(cardRevealSpan, drinkRecipe, drinkIngredients);
-    $(drinkDiv).append(cardImgDiv, cardContentDiv, cardRevealDiv);
+    getIngredients(drinks, drinkIngredients);
+
+    cardRevealDiv.append(cardRevealSpan, drinkRecipe, drinkIngredients);
+    drinkDiv.append(cardImgDiv, cardContentDiv, cardRevealDiv);
     $(".coctailsList").append(drinkDiv);
   });
 }
@@ -192,22 +180,20 @@ function searchByIngredients(event) {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(onSearchByIngredients);
-}
-
-function onSearchByIngredients(response) {
-  debugger;
-  var drinks = response.drinks;
-  if (drinks == "None Found") {
-    var img = $(".img").attr("src", "sad-cartoon-margarita.png");
-    $(".coctailsList")
-      .empty()
-      .html("<h4>None Found</h4>")
-      .append(img);
-  } else {
-    for (var i = 0; i < drinks.length; i++) {
-      var id = drinks[i].idDrink;
-      getInfo(id);
+  }).then(function(response) {
+    debugger;
+    var drinks = response.drinks;
+    if (drinks == "None Found") {
+      var img = $(".img").attr("src", "sad-cartoon-margarita.png");
+      $(".coctailsList")
+        .empty()
+        .html("<h4>None Found</h4>")
+        .append(img);
+    } else {
+      for (var i = 0; i < drinks.length; i++) {
+        var id = drinks[i].idDrink;
+        getInfo(id);
+      }
     }
-  }
+  });
 }

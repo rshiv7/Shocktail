43ea05b12210f4a817166d19a4f3b66f473cbd6b
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-14 18:48:36 -0700

    Adding more user routes in user-api-routes.js

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -3,18 +3,27 @@ var db = require("../models");
 
 module.exports = function(app){
 
-    // Returns all the data of the users
+    // Find all users and return to user as a json object
     app.get("/api/users", function(req, res){
-        db.user.findAll({
-            include: [db.Post]
-        }).then(function(dbUsers){
-            res.json(dbUsers);
+        db.user.findAll({}).then(function(dbUser){
+            res.json(dbUser);
+        });
+    });
+
+    // Find one user with id in req.params.id and return as json object
+    app.get("/api/users/:id", function(req, res) {
+        db.user.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbUser) {
+            res.json(dbUser);
         });
     });
 
     // Adds a new user to the model in mysql
     app.post("/api/users", function(req, res){
-        db.user.create(req.body).then(function(dbUsers){
+        db.user.create(req.body).then(function(dbUser){
             res.json(dbUser);
         });
     });

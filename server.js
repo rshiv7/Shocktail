19f10577f8e26357f673fe2d190360e484b211d2
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-16 00:41:15 -0700

    Adding requires for all routes within server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 
-
 // Sets up the Express App
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -15,7 +14,10 @@ app.use(express.static("public"));
 
 // Routes
 require("./routes/html-routes.js")(app);
-
+require("./routes/user-api-routes.js")(app);
+require("./routes/cocktail-api-routes.js")(app);
+require("./routes/reviews-api-routes.js")(app);
+require("./routes/pref-api-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {

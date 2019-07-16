a4b1ef324d05469d2e16c75a92afcd163a0b1ee3
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-15 23:13:20 -0700

    Adding a get call for a single review within reviews-api-routes.js

--- routes/reviews-api-routes.js
+++ routes/reviews-api-routes.js
@@ -4,18 +4,30 @@ var db = require("../models");
 module.exports = function (app) {
 
     //it will return all reviews
-    app.get("/api/review", function (req, res) {
-        db.review.findAll({}).then(
+    app.get("/api/reviews", function (req, res) {
+        db.Review.findAll({}).then(
             function (dbReview) {
                 res.json(dbReview);
             }
         )
     });
 
+    app.get("/api/reviews/:id", function(req, res) {
+        db.Review.findOne({
+            where: {
+                id: req.params.id
+            },
+            include: [db.User]
+        }).then(function(dbReview) {
+            console.log(dbReview);
+            res.json(dbReview);
+        });
+    });
+
      //user can add new review from db
-     app.post("/api/review", function(req, res){
-        db.review.create(req.body).then(function (dbReview){
-            res.json(dbreview);
+     app.post("/api/reviews", function(req, res){
+        db.Review.create(req.body).then(function (dbReview){
+            res.json(dbReview);
         });
     });
     
